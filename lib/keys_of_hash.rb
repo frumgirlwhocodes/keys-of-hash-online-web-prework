48f73d021f834088ed23369beaf66d0bab354d02
lieby weintraub <liebahinda@gmail.com>
2019-03-03 03:31:01 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,13 @@
+require "pry"
 class Hash
   def keys_of(*arguments)
     # code goes here
     new_array=[]
     self.each do |k,v|
       if arguments.include?(v)
+        new_array << v 
+        binding.pry
+      end
+      new_array
   end
 end
